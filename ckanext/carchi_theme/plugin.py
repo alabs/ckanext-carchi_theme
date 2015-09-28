6a88a9dfcfd020dd62087865251242cdeb3f2717
Andrés Pereira <apereira@alabs.es>
2015-09-28 11:52:32 -0500

    [i18n] test mo/pot

--- ckanext/carchi_theme/plugin.py
+++ ckanext/carchi_theme/plugin.py
@@ -1,13 +1,38 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
+def most_popular_groups():
+    '''Return a sorted list of the groups with the most datasets.'''
 
-class Carchi_ThemePlugin(plugins.SingletonPlugin):
+    # Get a list of all the site's groups from CKAN, sorted by number of
+    # datasets.
+    groups = toolkit.get_action('group_list')(
+        data_dict={'sort': 'package_count desc', 'all_fields': True})
+
+    # Truncate the list to the 6 most popular groups only.
+    groups = groups[:6]
+
+    return groups
+
+class Carchi_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
-
-    # IConfigurer
+    plugins.implements(plugins.ITranslation)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'carchi_theme')
+
+    def get_helpers(self):
+        '''Register the most_popular_groups() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'carchi_theme_most_popular_groups': most_popular_groups}
+
+
--- setup.py
+++ setup.py
@@ -79,5 +79,15 @@ setup(
     entry_points='''
         [ckan.plugins]
         carchi_theme=ckanext.carchi_theme.plugin:Carchi_ThemePlugin
+        [babel.extractors]
+        ckan = ckan.lib.extract:extract_ckan
     ''',
+
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    }
 )
